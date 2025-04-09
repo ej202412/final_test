bf9156bbf3346fc013f43327dddb2e26071b6084
ej202412 <dreamsin2010@gmail.com>
2025-04-10 01:56:27 +0900

    Update TeacherController.java

--- controller/TeacherController.java
+++ controller/TeacherController.java
@@ -19,24 +19,24 @@ public class TeacherController {
 	
 	@Autowired TeacherService teacherService;
 	
-	@GetMapping("/teacher")
+	@GetMapping("/teachers")
 	public List<TeacherDto> getTeacherList(){
 		return teacherService.getAllTeachers();
 	}
 	
-	@PostMapping("/teacher/add")
+	@PostMapping("/teachers")
 	public String addTeacher(@RequestBody TeacherDto dto) {
 		teacherService.updateTeacher(dto);
 		return "success!";
 	}
 	
-	@PatchMapping("/teacher/edit")
+	@PatchMapping("/teachers/{id}")
 	public String editTeacher(@RequestBody TeacherDto dto) {
 		teacherService.updateTeacher(dto);
 		return "success!";
 	}
 	
-	@DeleteMapping("/teacher/delete")
+	@DeleteMapping("/teachers/{id}")
 	public String deleteTeacher(@PathVariable int id) {
 		teacherService.deleteTeacher(id);
 		return "success!";
